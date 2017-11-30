79473341f091d123b9365cd6097051ec5014ddd0
An4na5 <mario.rada@uzh.ch>
2017-11-30 14:07:58 +0100

    Add existing files.

---
+++ task 2/customer.py
@@ -0,0 +1,41 @@
+from vehicle import *
+from random import randint
+
+class Customer(object):
+    
+    def __init__(self,name):
+        self.__name = name
+        self.__score = self.credit_score()
+
+    def __str__(self):
+        return "Customer: " + self.__name
+
+    def credit_score(self):
+        random_integer = randint(0, 100)
+        if random_integer > 60:
+            return True
+        else:
+            return False
+
+    def get_score(self):
+        return self.__score
+
+
+class VIP_Customer(Customer):
+
+    def credit_score(self):
+        return True
+
+
+### test cases ###
+
+# initialising customer instances
+
+Wendy = Customer("Wendy")
+Heidi = VIP_Customer("Heidi")
+
+print(Wendy) # expected output: Customer: Wendy
+print(Heidi) # expected output: Customer: Heidi
+
+print(Wendy.get_score()) # expected output: True
+print(Heidi.get_score()) # expected output: True
